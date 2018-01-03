bea3ca51b820fdc1f7a1a6e79c431109630d703e
William <he.william@yahoo.com>
2018-01-03 14:33:20 -0800

    added solutions to vowels, interative and using includes

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,35 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//     let vowels = ['a', 'e', 'i','o','u'];
+//     let counter = 0;
+//     let array = str.toLowerCase().split("")
+//     console.log(array)
+
+//     for(char of array){
+//         for(vowel of vowels){
+//             if(vowel === char) {
+//                 counter++
+//             }
+//         }
+//     }
+//     return counter
+// }
+
+function vowels(str) { //using includes
+    let vowels = 'aeiou';
+    let counter = 0;
+    let array = str.toLowerCase().split('')
+
+    for(char of array){
+        if(vowels.includes(char)) {
+            counter++
+        }
+    }
+    return counter
+}
+
+console.log(vowels("Hello My Name is William"))
 
 module.exports = vowels;
