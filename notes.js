1f3ec392308f358fd376d72c4b1198a2ba779230
William <he.william@yahoo.com>
2018-01-06 23:14:54 -0800

    added time complex notes

---
+++ notes.js
@@ -0,0 +1,17 @@
+// run time complexity
+
+// 1. constant time ** 1
+//     1. no matter how many elements were working with, the algorithm/operation will always take the same time.
+// 2. Log time ** log(n)
+//     1. you have this if doubling the number of element you are iterating over does NOT doubt the amount of work
+//         1. SEARCHING a sorted array
+// 3. linear time ** n
+//     1. iterating thru all elements in a collection of data. if you see a for loop from ‘0 to array.length’, you probably have linear time
+// 4. quasilinear **  n log (n)
+//         1. you have this if doubling the number of elements you are iterating over doesn’t double the work
+//             1. assume sorting operations are nlog(n)
+// 5. quad time ** n^2
+//     1. every element in a collection has to be compared to every other element
+//         1. handshake problem
+// 6. exponential 2^n
+//     1. if you had a single element in collection, the processing power required doubles
