cc287717e1e3edb3cec10448feb5c8a68be79941
William <he.william@yahoo.com>
2018-01-10 15:55:32 -0800

    added memoize

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -32,12 +32,27 @@
 //     return array[n]
 // }
 
-function fib(n) {
+function fib(n) { //2^n exponential time
     if( n < 2 ) {
         return n
     }
 
     return fib(n-2) + fib(n-1)
+}
+
+function memoize(fn) {
+   const cache = {};
+
+   return function(...args) {
+      if (cache[args]) {
+          return cache[args]
+      }
+
+      const result = fn.apply(this, args);
+      cache[args] = result;
+
+      return result;
+   }
 }
 
 console.log(fib(4))
