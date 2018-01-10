f491358052c2db59b961dd9eb30b363475f7a0f1
William <he.william@yahoo.com>
2018-01-10 14:41:32 -0800

    added iterative solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -7,7 +7,33 @@
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
+//   fib(2) === 1
 
-function fib(n) {}
+function fib(n) {
+    let array = [0, 1]
+    let index = 2;
+
+    if(n === 0) {
+        return 0
+    } 
+    if(n === 1 ) {
+        return 1
+    }
+
+    while(n >= index) {
+        array.push(array[index-1] + array[index-2])
+        index++
+    }
+    
+    return array[n]
+
+    // if(n === index) {
+    //     console.log('ran')
+    //     return array[index]
+    // } 
+
+}
+
+console.log(fib(6))
 
 module.exports = fib;
