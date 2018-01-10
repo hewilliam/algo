36ca3012d58ad971457281783230b0511956de55
William <he.william@yahoo.com>
2018-01-10 15:09:00 -0800

    added recursive solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -21,17 +21,25 @@
 //     return array[n]
 // }
 
-function fib(n) {
-    let array = [0,1]
+// function fib(n) {
+//     let array = [0,1]
 
-    for(i = 2; i <= n; i++) {
-        let a = array[i-1]
-        let b = array[i-2]
-        array.push(a+b)
+//     for(i = 2; i <= n; i++) {
+//         let a = array[i-1]
+//         let b = array[i-2]
+//         array.push(a+b)
+//     }
+//     return array[n]
+// }
+
+function fib(n) {
+    if( n < 2 ) {
+        return n
     }
-    return array[n]
+
+    return fib(n-2) + fib(n-1)
 }
 
-console.log(fib(3))
+console.log(fib(4))
 
 module.exports = fib;
