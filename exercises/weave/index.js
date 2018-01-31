d6a5197450bd80e4c114ad8d4fd0b7375b89041a
William <he.william@yahoo.com>
2018-01-31 13:04:25 -0800

    redid weave

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -25,19 +25,17 @@
 const Queue = require('./queue');
 
 function weave(sourceOne, sourceTwo) {
-    const q = new Queue();
-  
-    while (sourceOne.peek() || sourceTwo.peek()) {
-      if (sourceOne.peek()) {
-        q.add(sourceOne.remove());
-      }
-  
-      if (sourceTwo.peek()) {
-        q.add(sourceTwo.remove());
-      }
+  const q = new Queue();
+
+  while(sourceOne.peek() || sourceTwo.peek()) {
+    if(sourceOne.peek()) {
+      q.add(sourceOne.remove())
     }
-  
-    return q;
+    if(sourceTwo.peek()) {
+      q.add(sourceTwo.remove())
+    }
   }
+  return q
+}
 
 module.exports = weave;
